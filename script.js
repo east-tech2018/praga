857b68958264866b432ae59da8858233724365e6
Alexey_kr <dachadybenci2@gmail.com>
2018-09-03 17:56:38 +0300

    dev2 upadate

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
+// $('.event-slider').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+// });
+
 $('.event-slider').slick({
-    infinite: true,
+
+    speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
+
+
+
+
 
 $(document).ready(function () {
 
