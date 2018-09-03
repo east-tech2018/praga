303e2a68f05df97f221ad3609654cf4d92935ba1
LordAlexey <37488043+LordAlexey@users.noreply.github.com>
2018-09-03 21:54:36 +0300

    изменил чекбоксы и инпуты

--- script.js
+++ script.js
@@ -11,22 +11,14 @@ $('.event-slider').slick({
     slidesToScroll: 3,
     responsive: [
         {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
+            breakpoint: 1150,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 768,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
